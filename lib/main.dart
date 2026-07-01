a59c30d528b9a14c9165dec842a500f08146544e
Jnimatuj <jose.nimatuj02@gmail.com>
2026-07-01 16:42:18 -0600

    prueba commit 1

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//prueba de un commit
+
 void runProject() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Env.initialize();
